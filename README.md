39b8ead0a841ceffea84f8a4b32439af5a23ae43
mohsen-Nour <mohsen.noor@gmail.com>
2022-05-15 10:15:22 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-gaz8mn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-gaz8mn)
---
+++ index.js
@@ -0,0 +1,19 @@
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
+
+var a = 1;
+function sumOfSquare(num) {
+  var sum = 0;
+  for (var i = 0; i < num; i++) {
+    sum = sum + Math.sqrt(i);
+  }
+  return sum;
+}
+
+function approxSumOfSquare(num) {
+  return (2 / 3) * (Math.pow(num, 3 / 2) - 1);
+}
+
+console.log(sumOfSquare(11));
+console.log(approxSumOfSquare(11));
